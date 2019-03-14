f38e0acead02149dc3a2355944371f378c511506
sancarbar <sancarbar@gmail.com>
2019-03-14 13:17:43 -0500

    Take picture.

--- app/src/main/java/com/sancarbar/introduction/MainActivity.kt
+++ app/src/main/java/com/sancarbar/introduction/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.sancarbar.introduction
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    val REQUEST_IMAGE_CAPTURE = 1
+
+
+    //https://developer.android.com/training/camera/photobasics
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data!!.extras.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
+        }
+    }
+
+    fun onImageClicked(view: View) {
+        dispatchTakePictureIntent()
     }
 }
